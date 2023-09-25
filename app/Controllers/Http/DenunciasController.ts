c7e1e66589a6dc120bd44bc8632c7e3af92e68fb
BuracoZero <buracozero.oficial@gmail.com>
2023-09-25 12:23:43 -0300

    sim

---
+++ app/Controllers/Http/DenunciasController.ts
@@ -0,0 +1,15 @@
+// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Denuncia from '../../Models/Denuncia'
+import DenunciaValidator from '../../Validators/DenunciaValidator'
+
+export default class DenunciasController {
+
+  public async index({}: HttpContextContract) {}
+
+  public async store({}: HttpContextContract) {}
+
+  public async show({}: HttpContextContract) {}
+
+  public async destroy({}: HttpContextContract) {}
+}
