dbc687d47241e4040688f13e966a6cb8866c046b
mariaeduarda2504 <mariaeduardad391@gmail.com>
2023-11-27 20:04:54 -0300

    oi

---
+++ database/migrations/1700699255827_denuncias.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'denuncias'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('cep', 255).notNullable()
+      table.string('endereco', 255).notNullable().unique()
+      table.string('descricao', 180).notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
